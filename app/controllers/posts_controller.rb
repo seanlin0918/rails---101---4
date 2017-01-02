66c94d04b8e8d831d5c1a7b28407e7d31c733f3f
sean lin <seanlin0918@gmail.com>
2017-01-02 12:22:53 +1000

    up to date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,11 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
@@ -20,6 +25,25 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      redirect_to account_posts_path(@group), alert: "Post deleted"
+    else
+      render :edit
+    end
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    flash[:notice] = "Update success"
+
+    redirect_to account_posts_path(@group)
+  end
 
   private
 
